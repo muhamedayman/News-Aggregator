ef76a100ed677978763165130fbf753ed7b9314d
muhamedayman <34426019+muhamedayman@users.noreply.github.com>
2020-05-20 19:16:08 +0200

    Delete runPython.php

--- runPython.php
+++
@@ -1,9 +0,0 @@
-<?php
-include'includee.html';
-echo shell_exec("python C:\wamp64\cgi-bin\webScrapingCGI.py 2>&1");
-//echo shell_exec("python D:/SummarizationGrad/output.py 2>&1");
-
-include'footer.html';
-
-
-?>
